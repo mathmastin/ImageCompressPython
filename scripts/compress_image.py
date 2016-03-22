aba60bf87a64c63572f398c45b29b59a1dfaf1be
Matt Mastin <math.mastin@gmail.com>
2016-03-22 16:43:57 -0400

    Add scaffolding for initial training and compression scripts.

---
+++ scripts/compress_image.py
@@ -0,0 +1,18 @@
+"""Compress an image using the training matrix in the provided directory.
+
+Usage:
+  compress_image.py <filename> <directoryname>
+
+"""
+import sys
+sys.path.append('')
+
+from docopt import docopt
+
+
+def run(args):
+    pass
+
+
+if __name__ == '__main__':
+    run(docopt(__doc__))
---
+++ scripts/train_compression.py
@@ -0,0 +1,18 @@
+"""Train the compression algorithm using the training set in the provided directory.
+
+Usage:
+  compress_image.py <directoryname>
+
+"""
+import sys
+sys.path.append('')
+
+from docopt import docopt
+
+
+def run(args):
+    pass
+
+
+if __name__ == '__main__':
+    run(docopt(__doc__))
