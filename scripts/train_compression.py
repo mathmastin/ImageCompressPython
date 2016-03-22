66ba7d2edb0138f02f0e2330f0dc5f98f68138b1
Matt Mastin <math.mastin@gmail.com>
2016-03-22 17:56:54 -0400

    squash me

--- scripts/train_compression.py
+++ scripts/train_compression.py
@@ -8,10 +8,24 @@ import sys
 sys.path.append('')
 
 from docopt import docopt
+from skimage import io as image_io
+from skimage import img_as_uint
+from skimage import color
+import os
 
 
 def run(args):
-    pass
+    directory = args['<directoryname>']
+
+    image_names = []
+
+    for file in os.listdir(directory):
+        if file.endswith(".jpg"):
+            image_names.append(directory + file)
+
+    images = image_io.imread_collection(image_names)
+
+    grey_images = map(color.rgb2grey, images)
 
 
 if __name__ == '__main__':
