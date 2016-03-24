9cbe8d9c7887be62008683eccc21b509c4958d4c
Matt Mastin <math.mastin@gmail.com>
2016-03-24 17:15:28 -0400

    squash

--- scripts/compress_image.py
+++ scripts/compress_image.py
@@ -8,10 +8,51 @@ import sys
 sys.path.append('')
 
 from docopt import docopt
-
+import pickle
+from sklearn import linear_model
+from skimage import io as image_io
+from skimage import color
+from sklearn.feature_extraction.image import extract_patches_2d
+from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+from skimage import transform
+from numpy import reshape
+from numpy import matrix
 
 def run(args):
-    pass
+    filename = args['<filename>']
+    directoryname = args['<directoryname>']
+
+    with open(directoryname + 'compression.matrix', 'r') as infile:
+        compression_matrix = pickle.load(infile)
+
+    print len(compression_matrix)
+    print len(compression_matrix[0])
+    print compression_matrix[0]
+
+    image = transform.resize(color.rgb2grey(image_io.imread(filename)), (256, 192))
+
+    image_blocks = get_blocks(image)
+
+    image_vect = matrix(block_to_vect(image_blocks[0]))
+
+    print len(image_vect)
+
+    clf = linear_model.LinearRegression()
+
+    clf.fit(compression_matrix, image_vect.transpose())
+
+
+def block_to_vect(block):
+    row = [item for sublist in block for item in sublist]
+    return row
+
+
+def get_blocks(image):
+    return extract_patches_2d(image, (64, 64))
+
+
+def recover_block(vect):
+    return reshape(vect, (64, 64))
 
 
 if __name__ == '__main__':
--- scripts/train_compression.py
+++ scripts/train_compression.py
@@ -1,31 +1,82 @@
 """Train the compression algorithm using the training set in the provided directory.
 
 Usage:
-  compress_image.py <directoryname>
+  compress_image.py <directoryname> <ncomponents>
 
 """
 import sys
 sys.path.append('')
 
 from docopt import docopt
+from numpy import split
+from numpy import matrix
+from numpy import reshape
 from skimage import io as image_io
-from skimage import img_as_uint
+from skimage import img_as_ubyte
 from skimage import color
+from skimage import transform
+from skimage.viewer import ImageViewer
+from sklearn.feature_extraction.image import extract_patches_2d
+from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+from sklearn.decomposition import TruncatedSVD
+from scipy.linalg import svd
 import os
+import pickle
 
 
 def run(args):
     directory = args['<directoryname>']
+    num_components = int(args['<ncomponents>'])
 
     image_names = []
 
-    for file in os.listdir(directory):
-        if file.endswith(".jpg"):
-            image_names.append(directory + file)
+    for image_file in os.listdir(directory):
+        if image_file.endswith(".JPG"):
+            image_names.append(directory + image_file)
+
+    print 'Loading images...'
 
     images = image_io.imread_collection(image_names)
 
-    grey_images = map(color.rgb2grey, images)
+    print 'Resizing and greyscaling...'
+
+    images_processed = [transform.resize(color.rgb2grey(image), (256, 192)) for image in images]
+
+    print 'Blocking images...'
+
+    blocks = [get_blocks(image) for image in images_processed]
+
+    training_rows = []
+
+    print 'Building training matrix...'
+
+    for block in blocks[0]:
+        vect = block_to_vect(block)
+        training_rows.append(vect)
+
+    training_matrix = matrix(images_processed[0]).transpose()
+
+    print 'Computing SVD...'
+
+    trun_svd = TruncatedSVD(n_components=num_components)
+
+    compression_matrix = trun_svd.fit_transform(training_matrix)
+
+    with open(directory + 'compression.matrix', 'w') as outfile:
+        pickle.dump(compression_matrix, outfile)
+
+
+def block_to_vect(block):
+    row = [item for sublist in block for item in sublist]
+    return row
+
+
+def get_blocks(image):
+    return extract_patches_2d(image, (64, 64))
+
+
+def recover_block(vect):
+    return reshape(vect, (64, 64))
 
 
 if __name__ == '__main__':
